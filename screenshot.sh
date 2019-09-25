d159cbd072927fa45bee9678083b5430dd172ecb
Solarinas <solarinass@protonmail.com>
2019-09-25 18:11:04 -0400

    Added comments to screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -8,12 +8,15 @@ FILENAME=$SCREENSHOTS_DIR/$(date +%F_%H%M%S).png
 if [[ "$1" = "-s" ]]; then
     maim -o -u -d 1 -s -m 3 $FILENAME
 
+# Screenshot active window
 elif [[ $1 = "-u" ]]; then
     maim -u -d 1 -i $(xdotool getactivewindow) -m 3 $FILENAME
 
+# Screenshot primary desktop
 elif [[ $1 = "-m" ]]; then
     maim -u -d 1 -g 1920x1080 -m 3 $FILENAME
 
+# Temporary screenshot of whole desktop
 else
     maim /tmp/screenshot.png 
     xclip -selection clipboard -t image/png /tmp/screenshot.png
