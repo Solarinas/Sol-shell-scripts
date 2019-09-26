f3a29949819916afe3a3f6937956ee2542eb3623
Solarinas <solarinass@protonmail.com>
2019-09-25 22:30:59 -0400

    Added notifications for when screenshots are taken

--- screenshot.sh
+++ screenshot.sh
@@ -2,24 +2,35 @@
 
 SCREENSHOTS_DIR=~/Pictures/Screenshots
 FILENAME=$SCREENSHOTS_DIR/$(date +%F_%H%M%S).png
-
+NOTIFICATION_PICTURE="~/.scripts/gabriel-notification-icons/proxy.duckduckgo.com.jpg"
 
 # Screenshot selection
 if [[ "$1" = "-s" ]]; then
     maim -o -u -d 1 -s -m 3 $FILENAME
+    xclip -selection clipboard -t image/png $FILENAME
 
 # Screenshot active window
 elif [[ $1 = "-u" ]]; then
+    notify-send "Why do you hate me?" --urgency low -i $NOTIFICATION_PICTURE 
     maim -u -d 1 -i $(xdotool getactivewindow) -m 3 $FILENAME
+    xclip -selection clipboard -t image/png $FILENAME
 
 # Screenshot primary desktop
 elif [[ $1 = "-m" ]]; then
+    notify-send "Why do you hate me?" --urgency low -i $NOTIFICATION_PICTURE
     maim -u -d 1 -g 1920x1080 -m 3 $FILENAME
+    xclip -selection clipboard -t image/png $FILENAME
 
 # Temporary screenshot of whole desktop
-else
+elif [ -z "$1" ]; then
+    notify-send "Why do you hate me?" --urgency low -i $NOTIFICATION_PICTURE
     maim /tmp/screenshot.png 
     xclip -selection clipboard -t image/png /tmp/screenshot.png
+
+# Display error if command fails
+else
+    notify-send "You made a fucksy wucksy" -i $NOTIFICATION_PICTURE 
 fi
+
     
     
