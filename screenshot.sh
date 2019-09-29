37b15fcade0f57b5f0d25a704788de65ba541091
Solarinas <solarinass@protonmail.com>
2019-09-29 17:19:23 -0400

    Changed notification message to something more weeb like

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 SCREENSHOTS_DIR=~/Pictures/Screenshots
 FILENAME=$SCREENSHOTS_DIR/$(date +%F_%H%M%S).png
@@ -6,24 +6,25 @@ NOTIFICATION_PICTURE="~/.scripts/gabriel-notification-icons/proxy.duckduckgo.com
 
 # Screenshot selection
 if [[ "$1" = "-s" ]]; then
+    notify-send "Screenshot taken Goshujin-sama~" --urgency low -i $NOTIFICATION_PICTURE 
     maim -o -u -d 1 -s -m 3 $FILENAME
     xclip -selection clipboard -t image/png $FILENAME
 
 # Screenshot active window
 elif [[ $1 = "-u" ]]; then
-    notify-send "Why do you hate me?" --urgency low -i $NOTIFICATION_PICTURE 
+    notify-send "Screenshot taken Goshujin-sama~" --urgency low -i $NOTIFICATION_PICTURE 
     maim -u -d 1 -i $(xdotool getactivewindow) -m 3 $FILENAME
     xclip -selection clipboard -t image/png $FILENAME
 
 # Screenshot primary desktop
 elif [[ $1 = "-m" ]]; then
-    notify-send "Why do you hate me?" --urgency low -i $NOTIFICATION_PICTURE
+    notify-send "Screenshot taken Goshujin-sama~" --urgency low -i $NOTIFICATION_PICTURE
     maim -u -d 1 -g 1920x1080 -m 3 $FILENAME
     xclip -selection clipboard -t image/png $FILENAME
 
 # Temporary screenshot of whole desktop
 elif [ -z "$1" ]; then
-    notify-send "Why do you hate me?" --urgency low -i $NOTIFICATION_PICTURE
+    notify-send "Screenshot taken Goshujin-sama~" --urgency low -i $NOTIFICATION_PICTURE
     maim /tmp/screenshot.png 
     xclip -selection clipboard -t image/png /tmp/screenshot.png
 
@@ -31,6 +32,3 @@ elif [ -z "$1" ]; then
 else
     notify-send "You made a fucksy wucksy" -i $NOTIFICATION_PICTURE 
 fi
-
-    
-    
