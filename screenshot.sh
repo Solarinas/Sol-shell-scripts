c731f728d23a880ff8de5312f5166067cae6f109
Solarinas <solarinass@protonmail.com>
2020-03-18 11:57:13 -0400

    Quoted variables in screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -12,17 +12,17 @@ function after_screenshot {
 }
 
 if [[ "$1" = "-s" ]]; then
-    maim -o -u -d 1 -s -m 3 $FILENAME
+    maim -o -u -d 1 -s -m 3 "$FILENAME"
     after_screenshot
 elif [[ $1 = "-u" ]]; then
-    maim -u -d 1 -i $(xdotool getactivewindow) -m 3 $FILENAME
+    maim -u -d 1 -i "$(xdotool getactivewindow)" -m 3 "$FILENAME"
     after_screenshot
 elif [[ $1 = "-m" ]]; then
-    maim -u -d 1 -g 1920x1080 -m 3 $FILENAME
+    maim -u -d 1 -g 1920x1080 -m 3 "$FILENAME"
     after_screenshot
 elif [ -z "$1" ]; then
     maim /tmp/screenshot.png
     after_screenshot
 else
-    notify-send "You made a fuck wucky" -i $COWBOY
+    notify-send "You made a fuck wucky" -i "$COWBOY"
 fi
