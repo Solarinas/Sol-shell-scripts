dd939b07efc1e2962d7b4a947236d1862ec521a6
Solarinas <solarinass@protonmail.com>
2020-03-19 15:02:42 -0400

    Shortend if statements, changed weird variable name

--- screenshot.sh
+++ screenshot.sh
@@ -5,24 +5,22 @@ FILENAME=$SCREENSHOTS_DIR/$(date +%F_%H%M%S).png
 #COWBOY="~/.emacs.d/GabEmacs2.png"
 
 function after_screenshot {
-    COWBOY="$HOME/.emacs.d/GabEmacs2.png"
+    ICON="$HOME/.emacs.d/GabEmacs2.png"
 
-    notify-send "Screenshot taken Goshujin-sama~" --urgency low -i "$COWBOY"
+    notify-send "Screenshot taken Goshujin-sama~" --urgency low -i "$ICON"
     xclip -selection clipboard -t image/png "$FILENAME"
 }
 
-if [[ "$1" = "-s" ]]; then
+if [ -s "$1" ]; then
     maim -o -u -d 1 -s -m 3 "$FILENAME"
     after_screenshot
-elif [[ $1 = "-u" ]]; then
+elif [ -u "$1" ]; then
     maim -u -d 1 -i "$(xdotool getactivewindow)" -m 3 "$FILENAME"
     after_screenshot
-elif [[ $1 = "-m" ]]; then
+elif [ -m "$1" ]; then
     maim -u -d 1 -g 1920x1080 -m 3 "$FILENAME"
     after_screenshot
 elif [ -z "$1" ]; then
     maim /tmp/screenshot.png
     after_screenshot
-else
-    notify-send "You made a fuck wucky" -i "$COWBOY"
 fi
